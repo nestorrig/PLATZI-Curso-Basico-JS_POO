370bde6b51591feaef1710746eed9d2108cb46be
nestorrig <neriga10@hotmail.com>
2023-10-10 21:25:38 -0600

    Clase 13: Módulos de ECMAScript 6

---
+++ module.mjs
@@ -0,0 +1,16 @@
+import { PlatziClass } from "./classes.mjs";
+
+const clase67 = new PlatziClass({
+    name: "JavaScript: orientado a objetos, basado en prototipos",
+    videoID: "wertyuio;p87htreghtyuioouyjtrh",
+});
+
+clase67.reproducir();
+clase67.pausar();
+
+clase67.reproducir();
+clase67.pausar();
+
+clase67.reproducir();
+clase67.pausar();
+
